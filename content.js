111653cc326424f9507f0d03911f48e125fb6921
Ealodi <75173518+Ealodi@users.noreply.github.com>
2024-05-10 20:34:00 +0800

    5.10

--- content.js
+++ content.js
@@ -3,6 +3,18 @@
 const container = document.createElement("div");
 container.classList.add("VisContainer");
 
+const containerTop = document.createElement("div");
+containerTop.classList.add("topContainer");
+
+const containerMid = document.createElement("div");
+containerMid.classList.add("midContainer");
+
+const containerBot = document.createElement("div");
+containerBot.classList.add("botContainer");
+
+container.appendChild(containerTop);
+container.appendChild(containerMid);
+container.appendChild(containerBot);
 // 关闭
 const closeButton = document.createElement("button");
 closeButton.textContent = "关闭";
@@ -10,20 +22,12 @@ closeButton.addEventListener("click", () => {
   container.style.display = "none";
 });
 
-container.appendChild(closeButton);
-
-
-// g27-68 创建空白的 div 元素，用于在表格上方创建空间
-const header = document.createElement("div");
-header.style.height = "90px"; // 设置空间的高度
-header.style.display = "flex"; // 使用 flex 布局
-header.style.flexDirection = "row"; // 主轴方向为水平方向
-header.style.justifyContent = "space-between"; // 子元素之间的间距分配在主轴上
-header.style.borderLeft = "10px solid transparent"; // 添加左边框，宽度为10像素，颜色为透明
+containerMid.appendChild(closeButton);
 
 // 创建三个子元素作为三块
 const block1 = document.createElement("div");
 block1.textContent = ""; // 给子元素添加内容
+block1.classList.add("leftBlock");
 block1.style.flex = "1"; // 子元素自动填充剩余空button
 
 const block2 = document.createElement("div");
@@ -31,42 +35,22 @@ block2.textContent = ""; // 给子元素添加内容
 block2.style.flex = "1"; // 子元素自动填充剩余空间
 
 const block3 = document.createElement("div");
+block3.classList.add("rightBlock");
 block3.textContent = ""; // 给子元素添加内容
 block3.style.flex = "1"; // 子元素自动填充剩余空间
 
 // 将三个子元素添加到 header 元素中
-header.appendChild(block1);
-header.appendChild(block2);
-header.appendChild(block3);
-
-// 将 header 元素添加到容器中，放在容器的第一个子元素位置
-container.insertBefore(header, container.firstChild);
-// 设置 block1 的样式属性
-block1.style.color = "#02D2C5"; // 设置颜色为红色
-block1.style.fontSize = "30px"; // 设置字体大小为16像素
-block1.style.fontWeight = "bold"; // 设置字体加粗
-block1.style.textAlign = "center"; // 设置文本居中显示
-
-// 设置 block3 的样式属性
-block3.style.color = "#40B1F1"; // 设置颜色为绿色
-block3.style.fontSize = "30px"; // 设置字体大小为18像素
-block3.style.fontWeight = "bold"; // 设置字体加粗
-block3.style.textAlign = "center"; // 设置文本居中显示
-// 将空间添加到容器中，放在容器的第一个子元素位置
-
+containerTop.appendChild(block1);
+containerTop.appendChild(block2);
+containerTop.appendChild(block3);
 
 // 将容器添加到页面中
 document.body.appendChild(container);
+
 // 显示
 const button = document.createElement("button");
+button.classList.add("startButton");
 button.textContent = "显示";
-button.style.position = "fixed";
-button.style.top = "10px";
-button.style.right = "10px";
-
-//g85-106 加了显示图标
-button.style.display = "flex"; // 使用 flex 布局
-button.style.alignItems = "center"; // 垂直居中按钮内容
 
 // 创建 SVG 图标
 const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -74,30 +58,26 @@ svgIcon.setAttribute("viewBox", "0 0 1024 1024");
 svgIcon.setAttribute("width", "20");
 svgIcon.setAttribute("height", "20");
 svgIcon.style.marginRight = "5px"; // 调整图标与按钮文本之间的间距
-
 // 创建 SVG 路径
 const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
 path.setAttribute("d", "M213.333 308.513c-23.564 0-42.666-19.103-42.666-42.667s19.102-42.667 42.666-42.667h597.334c23.564 0 42.666 19.103 42.666 42.667s-19.102 42.667-42.666 42.667H213.333z m0 262.564c-23.564 0-42.666-19.103-42.666-42.667s19.102-42.666 42.666-42.666h597.334c23.564 0 42.666 19.102 42.666 42.666 0 23.564-19.102 42.667-42.666 42.667H213.333z m0 262.564c-23.564 0-42.666-19.102-42.666-42.667 0-23.564 19.102-42.666 42.666-42.666h597.334c23.564 0 42.666 19.102 42.666 42.666 0 23.565-19.102 42.667-42.666 42.667H213.333z");
 path.setAttribute("fill", "#2c2c2c");
-
 // 将路径添加到 SVG 图标中
 svgIcon.appendChild(path);
-
 // 将 SVG 图标添加到按钮中
 button.appendChild(svgIcon);
-
 // 将按钮添加到文档主体中
-
 document.body.appendChild(button);
+
 // 添加按钮 左
 const detectButton = document.createElement("button");
 detectButton.textContent = "添加到左侧";
-container.appendChild(detectButton);
+containerMid.appendChild(detectButton);
 
 // 添加按钮 右
 const detectButtonRight = document.createElement("button");
 detectButtonRight.textContent = "添加到右侧";
-container.appendChild(detectButtonRight);
+containerMid.appendChild(detectButtonRight);
 
 
 var lastLeftValue = '';
@@ -105,7 +85,7 @@ var lastRightValue = '';
 // 清空列表
 const clearButton = document.createElement("button");
 clearButton.textContent = "清空";
-container.appendChild(clearButton);
+containerMid.appendChild(clearButton);
 
 clearButton.addEventListener("click", () => {
   chrome.storage.sync.clear();
@@ -131,25 +111,10 @@ button.addEventListener("click", () => {
   }
 });
 
-// 新建一个区域容纳表格
-const leftTextI = document.createElement("div");
-leftTextI.style.height = "600px";
-leftTextI.style.width = "580px";
-leftTextI.style.marginTop = "20px";
-leftTextI.style.marginLeft = "10px";
-leftTextI.style.overflowY = "auto";
-leftTextI.style.border = "1px solid #ccc";
-leftTextI.style.display = "flex";
-
 const table = document.createElement('table');
-table.style.width = "100%";
-table.style.height = "100%";
+table.classList.add("DataTable");
 //table.style.borderCollapse = 'collapse';
-leftTextI.appendChild(table);
-
-container.appendChild(leftTextI);
-
-
+containerBot.appendChild(table);
 
 
 // 从存储中获取数据
@@ -260,7 +225,7 @@ function writeData(infoBoxContent){
           var cell = row.insertCell();
           cell.textContent = cellData;
           cell.style.color = "black";
-          cell.style.border = '1px solid #ddd'; // 设置单元格边框
+          //cell.style.border = '1px solid #ddd'; // 设置单元格边框
           cell.style.padding = '8px'; // 设置单元格内边距
           // cell.style.textOverflow = 'ellipsis';
           // cell.style.overflow = 'hidden';
@@ -299,9 +264,8 @@ function mergeArrays(array1, array2) {
         //console.log(array2[i][1] + '\n' + name + '\n' + getSimilarity(name,array2[i][1]));
         if(!array2[i][1] || !array2[i][0])continue;
         else if(getSimilarity(name,array2[i][1]) >= 80){
-          const vlkw = array2[i][0];
-          array2.splice(i,1);
-          mergedArray.push([value, name, vlkw]);
+          //array2.splice(i,1);
+          mergedArray.push([value, name, array2[i][0]]);
         }
       }
     }
@@ -357,13 +321,34 @@ function fillPageTitle() {
     }
   }
 }
+function extractNumbersFromString(inputText) {
+  // 正则表达式匹配数字
+  const regex = /(?:^|\D)(\d+)(?![^\(]*\))(?![^\[]*\])(?![^\{]*\})(?:$|\D)/g;
+
+  // 匹配到的数字数组
+  const numbers = [];
+  
+  // 在输入文本中匹配数字
+  let match;
+  while ((match = regex.exec(inputText)) !== null) {
+      numbers.push(match[1]);
+  }
+
+  // 如果找到数字并且不是日期、编码或两个数字，则返回找到的数字
+  if (numbers.length === 1 && inputText.split(',').length <= 2) {
+      return "Numbers found: " + numbers.join(", ");
+  } else {
+      // 否则返回空字符串
+      return "";
+  }
+}
 // 关闭按钮的点击事件处理程序
 closeButton.addEventListener("click", () => {
   // 隐藏容器
-  container.style.display = "none";
+  container.style.visibility = "hidden";
 });
-// 显示按钮的点击事件处理程序
+//显示按钮的点击事件处理程序
 button.addEventListener("click", () => {
   // 将容器的样式设置为显示
-  container.style.display = "block";
+  container.style.visibility = "visible";
 });
