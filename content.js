f1c502c4f4c0194e9b0e56e05555e47402ab3b79
Ealodi <75173518+Ealodi@users.noreply.github.com>
2024-07-15 15:44:32 +0800

    7.15

--- content.js
+++ content.js
@@ -1,22 +1,54 @@
-// background.js
 
-function injectD3() {
-  return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      // 设置 script 元素的 src 属性为 D3.js 文件的 URL
-      script.src = chrome.runtime.getURL('d3.v5.min.js');
-      script.onload = resolve;
-      script.onerror = reject;
-      // 将 script 元素添加到页面中
-      (document.head || document.documentElement).appendChild(script);
-  });
-}
-// 注入 D3.js 并发送消息给 content script
-injectD3().then(() => {
-  console.log("d3 注入成功");
-  const script = document.createElement('script');
-  // 设置脚本的 src 属性为你想要注入的脚本的 URL
-  script.src = chrome.runtime.getURL('main.js');
-  // 将脚本插入到页面中的 <head> 元素中
-  document.body.appendChild(script);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "getLabelAndValue") {
+      // 发送页面的 HTML 内容给 popup.js
+      const data = getLabelAnValue();
+      console.log(data);
+      sendResponse({processedData: data});
+    }
 });
+
+function getLabelAnValue(){
+  // 获取当前页面的所有属性值和属性名
+  // 选择具有特定类名的元素
+  const infobox = document.querySelector('.infobox');
+  // 获取infobox的tbody子元素
+  const tbodyChildren = infobox.querySelector('tbody');
+  // 获取tbody下的第一个tr元素
+  var Tr = tbodyChildren.querySelector('tr');
+  // 获取关键词
+  const name_ = document.querySelector(".mw-page-title-main").textContent;
+  const valueLabelsBox = [];
+  valueLabelsBox.push([name_,'keyword_'])
+  var toprow = "";
+  while(Tr){
+    const th = Tr.querySelector('th');
+    const td = Tr.querySelector('td');
+    var parentClassName;
+    if (th != null)parentClassName = th.parentElement.className;
+    else parentClassName = null;
+    if (parentClassName != null) {
+      //console.log(th + '\n' + td);
+      var label = RemakeValue(th.textContent);
+      var value = "";
+      if(td != null)value = RemakeValue(td.textContent);
+      // 如果是头元素
+      if(parentClassName == 'mergedtoprow')toprow = label;
+      else if (label[0] == '•'|| label[0] == '-')valueLabelsBox.push([value,toprow + label]);
+      else valueLabelsBox.push([value,label]);
+      console.log(toprow);
+    }
+    Tr = Tr.nextElementSibling;
+  }
+  console.log(valueLabelsBox);
+  return valueLabelsBox;
+}
+function RemakeValue(inputString) {
+  // 去除value中的无效字符
+  //console.log(inputString);
+  const withoutComments = inputString.replace(/\/\*[\s\S]*?\*\//g, '');
+  const withoutCSSRules = withoutComments.replace(/[^{]*\{[^}]+\}/g, '');
+  
+  return withoutCSSRules.replace(/\s+/g, "");;
+}
